ef1ffb30d3cc13abfa464b0951ab051f0ae8cb1a
Xreddr <redwardhenion@gmail.com>
2021-12-11 20:12:24 -0800

    Initial commit

---
+++ blackjack.js
@@ -0,0 +1,30 @@
+
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+
+
+function getDeck() {
+    let deck = new Array();
+    for(let i = 0; i < suits.length; i++) {
+        for(let x = 0; x < values.length; x++) {
+            let card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+    return deck;
+}
+
+function shuffle(deck) {
+    for (let i = 0; i < 1000; i++) {
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let tmp = deck[location1];
+        
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
+    }
+}
+
+getDeck();
+
