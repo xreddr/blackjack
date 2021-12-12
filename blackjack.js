c3c81c52607bb5d137efc16d132d6f869c41ef29
Xreddr <redwardhenion@gmail.com>
2021-12-12 11:28:09 -0800

    Testing card render basics

--- blackjack.js
+++ blackjack.js
@@ -1,8 +1,5 @@
-
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
-
 
 function getDeck() {
     let deck = new Array();
@@ -16,7 +13,7 @@ function getDeck() {
 }
 
 function shuffle(deck) {
-    for (let i = 0; i < 1000; i++) {
+    for(let i = 0; i < 1000; i++) {
         let location1 = Math.floor((Math.random() * deck.length));
         let location2 = Math.floor((Math.random() * deck.length));
         let tmp = deck[location1];
@@ -24,7 +21,40 @@ function shuffle(deck) {
         deck[location1] = deck[location2];
         deck[location2] = tmp;
     }
+    return deck;
+}
+
+function render(deck) {
+    document.getElementById("demo").innerHTML = "";
+    for(let i = 0; i < deck.length; i++) {
+        let card = document.createElement("div");
+        let value = document.createElement("div");
+        let suit = document.createElement("div");
+        card.className = "card";
+        value.className = "value";
+        suit.className = "suit";
+        
+        value.innerHTML = deck[i].Value;
+        card.appendChild(value);
+        card.appendChild(suit);
+        
+        document.getElementById("demo").appendChild(card);
+    }
 }
 
 getDeck();
+
+function testRender() {
+    const para = document.createElement("div");
+    //const node = document.createTextNode("Hello");
+    para.className = "card";
+    //para.appendChild(node);
+
+    const element = document.getElementById("demo");
+    element.appendChild(para);
+}
+
+testRender();
+render(shuffle(getDeck()));
+
 
