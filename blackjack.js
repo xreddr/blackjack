e98707bf59b85ea1e1b89925eeefef082e467d84
Xreddr <redwardhenion@gmail.com>
2021-12-18 23:36:53 -0800

    Scoring function works but needs to change ace value

--- blackjack.js
+++ blackjack.js
@@ -70,15 +70,35 @@ function render(cards, l) {
     }
 }
 
+
+
 function score(player) {
     let playerScore = 0;
     let points = {
         "K": 10,
+        "Q": 10,
         "J": 10,
+        "10": 10,
+        "9": 9,
+        "8": 8,
+        "7": 7,
+        "6": 6,
+        "5": 5,
+        "4": 4,
+        "3": 3,
+        "2": 2,
+        "A": 11
     };
     for(let i = 0; i < player.hand.length; i++) {
-        console.log(player.hand[i].Value); 
+        let s = player.hand[i].Value;
+        console.log(points[s]);
+        playerScore = playerScore + points[s];
+        
     }
+    if(playerScore > 21 && player.hand.Value == "A") {
+            playerScore = playerScore - 10;
+    }else {}
+    return playerScore;
 }
 
 render(deck, "deck");
